18c4ef3dcf968fe7994abd012ddedbfbcbdc8c33
MohammadAbdulGh <>
2021-07-14 16:17:25 +0000

    Adding rendering image

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ else{
   alert("you can check our selections")
 
 document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
+  "<div>" + "<h3>Arabic Selection</h3>"
 + "<img  src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' width='300' height='300' title='Arabic Selection'     />" + "</div>")
 
 document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
+  "<div>" + "<h3>English Selection</h3>"
 + "<img  src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' width='300' height='300' title='English Selection'     />" + "</div>")
 
 document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
+  "<div>" + "<h3>Global Selection</h3>"
 + "<img  src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' width='300' height='300' title='Global Selection'     />" + "</div>")
 
 
