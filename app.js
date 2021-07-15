54e9f7cb07b8fc0ee946f64a76ca95d5b309debe
MohammadAbdulGh <>
2021-07-15 12:40:55 +0000

    Added CSS File

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-var name = prompt("What is your name")
+// var name = prompt("What is your name")
 
-alert("Wlecome to World of Music " + name )
+// alert("Wlecome to World of Music " + name )
 
 
   
-  var r = confirm("Do you love Music" + name)
-  if (r == true) {
-    var favMusic = prompt("What is you favorite music type?")
-    if(favMusic == "Arabic"){
-alert("We have the perfect selection for you")
-document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
-+ "<img  src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' width='300' height='300' title='Arabic Selection'     />" + "</div>")
+//   var r = confirm("Do you love Music" + name)
+//   if (r == true) {
+//     var favMusic = prompt("What is you favorite music type?")
+//     if(favMusic == "Arabic"){
+// alert("We have the perfect selection for you")
+// document.write(
+//   "<div>" + "<h3>" + favMusic +"</h3>"
+// + "<img  src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' width='300' height='300' title='Arabic Selection'     />" + "</div>")
 
-}
-else if (favMusic =="English"){
-  alert("We have the perfect selection for you")
-document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
-+ "<img  src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' width='300' height='300' title='English Selection'     />" + "</div>")
-}
-else if (favMusic =="Global"){
-  alert("We have the perfect selection for you")
-document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
-+ "<img  src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' width='300' height='300' title='Global Selection'     />" + "</div>")
+// }
+// else if (favMusic =="English"){
+//   alert("We have the perfect selection for you")
+// document.write(
+//   "<div>" + "<h3>" + favMusic +"</h3>"
+// + "<img  src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' width='300' height='300' title='English Selection'     />" + "</div>")
+// }
+// else if (favMusic =="Global"){
+//   alert("We have the perfect selection for you")
+// document.write(
+//   "<div>" + "<h3>" + favMusic +"</h3>"
+// + "<img  src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' width='300' height='300' title='Global Selection'     />" + "</div>")
 
-}
-else{
-  alert("you can check our selections")
+// }
+// else{
+//   alert("you can check our selections")
 
-document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
-+ "<img  src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' width='300' height='300' title='Arabic Selection'     />" + "</div>")
+// document.write(
+//   "<div>" + "<h3>Arabic Selection</h3>"
+// + "<img  src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' width='300' height='300' title='Arabic Selection'     />" + "</div>")
 
-document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
-+ "<img  src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' width='300' height='300' title='English Selection'     />" + "</div>")
+// document.write(
+//   "<div>" + "<h3>English Selection</h3>"
+// + "<img  src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' width='300' height='300' title='English Selection'     />" + "</div>")
 
-document.write(
-  "<div>" + "<h3>" + favMusic +"</h3>"
-+ "<img  src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' width='300' height='300' title='Global Selection'     />" + "</div>")
+// document.write(
+//   "<div>" + "<h3>Global Selection</h3>"
+// + "<img  src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' width='300' height='300' title='Global Selection'     />" + "</div>")
 
 
-}
+// }
 
-  } else {
-    alert("We are sorry to hear that, but check our website you may change your mind ")
-  }
+//   } else {
+//     alert("We are sorry to hear that, but check our website you may change your mind ")
+//   }
   
 
 
