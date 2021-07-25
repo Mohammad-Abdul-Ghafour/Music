8db6521955329ded52d17b8efc9664a5647e218c
MohammadAbdulGh <>
2021-07-25 10:39:29 +0000

    Add loops

--- app.js
+++ app.js
@@ -2,21 +2,52 @@ var name = prompt("What is your name")
 
 alert("Wlecome to World of Music " + name)
 
-var r = confirm("Do you love Music" + name)
+
+
+
+// for (let i = 0; i < 2; i++){
+// var fovInstrument = prompt("What is my favorite instrument? \n 1.Guitar \n 2.Piano \n 3.Flute")
+// fovInstrument=fovInstrument.toLowerCase()
+// if (fovInstrument == "guitar"){
+//   alert("Oooh, you got me")
+//   i = 2
+// }
+// else if (i == 1){
+//   alert("the correct answer is guitar")
+
+// }
+// else if(fovInstrument != "guitar"){
+//   alert("oops,try to guess again")
+
+// }
+// }
+
+var fovInstrument = prompt("What is my favorite instrument? \n 1.Guitar \n 2.Piano \n 3.Flute")
+while(fovInstrument != "guitar"){
+  alert("oops,try to guess again")
+  fovInstrument = prompt("What is my favorite instrument? \n 1.Guitar \n 2.Piano \n 3.Flute")
+}
+
+
+
+
+
+var r = confirm("Do you love Music " + name)
 if (r == true) {
   var favMusic = prompt("What is you favorite music type?")
-  favMusic = favMusic.toLowerCase()
+  // if (favMusic!= null){
+//  favMusic = favMusic.toLowerCase()
   if (favMusic == "arabic") {
     alert("We have the perfect selection for you")
-    document.write("<section class='col'>" + "<div>"+"<h4>Arabic Selection</h4>"+"<img src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' alt='Arabic Selection' width='300' height='300' title='Arabic Selection''>"+"</div>"+"</section>")
+    document.write("<section class='col'>" + "<div class='col1'>"+"<h4>Arabic Selection</h4>"+"</div>" + "<div class='col1'>"+"<img src='https://image.winudf.com/v2/image/Y29tLmFyYWJtdXNpYy5zdHVkaW9zYXR1X3NjcmVlbl8wXzQwMzVzaHhq/screen-0.jpg?fakeurl=1&type=.jpg' alt='Arabic Selection' width='300' height='300' title='Arabic Selection''>"+"</div>"+"</section>")
   }
   else if (favMusic == "english") {
     alert("We have the perfect selection for you")
-    document.write("<section class='col'>" + "<div>"+"<h4>English Selection</h4>"+"<img src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' alt='English Selection' width='300' height='300' title='English Selection''>"+"</div>"+"</section>")
+    document.write("<section class='col'>" + "<div class='col1'>"+"<h4>English Selection</h4>"+"</div>" + "<div class='col1'>"+"<img src='https://icon-library.com/images/english-icon-png/english-icon-png-15.jpg' alt='English Selection' width='300' height='300' title='English Selection''>"+"</div>"+"</section>")
   }
   else if (favMusic == "global") {
     alert("We have the perfect selection for you")
-    document.write("<section class='col'>" + "<div>"+"<h4>Global Selection</h4>"+"<img src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' alt='Global Selection' width='300' height='300' title='Global Selection''>"+"</div>"+"</section>")
+    document.write("<section class='col'>" + "<div class='col1'>"+"<h4>Global Selection</h4>"+"</div>"+"<div class='col1'>"+"<img src='https://i1.sndcdn.com/avatars-000207568864-z2nrm4-t500x500.jpg' alt='Global Selection' width='300' height='300' title='Global Selection''>"+"</div>"+"</section>")
   }
   else {
     alert("you can check our selections")
@@ -28,3 +59,9 @@ if (r == true) {
 } else {
   alert("We are sorry to hear that, but check our website you may change your mind ")
 }
+  // }
+
+ 
+console.log('test')
+
+document.write("<script>var rate = prompt('Rate our website from 1 to 5');document.write('<section class="+"col"+"><div class="+"col1"+"><h4>'+rate+' out of 5 </h4></div></section>')</script><section class="+"col"+"><div class="+"col1"+"><script>for (let i1 = 0; i1<rate;i1++){document.write('<img src="+"https://starpng.com/public/uploads/preview/golden-star-png-21573888480jsiv6fmfyj.png"+" alt="+"Global Selection"+" width="+"300"+" height="+"300"+" title="+"Global Selection"+">')}</script></div></section>")
